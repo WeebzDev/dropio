c5a2c5b889b872c1b843f5ece0bc291161dc8f0e
CookyNdi <cookyndi2@gmail.com>
2025-05-10 12:03:51 +0700

    edit baseurl docusaurus config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: 'https://dropio.my.id',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/dropio/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -131,4 +131,3 @@ const config: Config = {
 };
 
 export default config;
-
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,6 @@ import type { ReactNode } from 'react';
 import { Redirect } from '@docusaurus/router';
 
 export default function Home(): ReactNode {
-  return <Redirect to='/docs/intro' />;
+  return <Redirect to='/dropio/docs/intro' />;
 }
 
