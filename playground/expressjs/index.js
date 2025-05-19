294dc90ac8587797f111be4916e17bcd1bedd074
CookyNdi <cookyndi2@gmail.com>
2025-05-19 13:17:48 +0700

    adding express js playground

---
+++ playground/expressjs/index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const diolib = require('./lib/dropio/server');
+const app = express();
+const port = 5010;
+
+const dio = diolib.createDropio();
+
+const ourFileRouter = {
+  fileUploader: dio({
+    image: {
+      maxFileCount: 1,
+      maxFileSize: '10MB',
+    },
+  }),
+};
+
+app.use(express.json());
+
+app.post('/api/dropio', (req, res) => {
+  const metadata = req.body;
+  const result = ourFileRouter.fileUploader(metadata);
+  res.status(200).json(result);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
