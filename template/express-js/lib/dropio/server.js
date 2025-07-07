a2789021da3ebaa8ac191c2357b4497b40f42fb7
CookyNdi <cookyndi2@gmail.com>
2025-07-07 10:35:12 +0700

    Add bucketDetails method to DIOApi class
    Introduces a new async bucketDetails method to the DIOApi class in express-js, express-ts, and nextjs-app templates. This method fetches bucket details from the ingest server, handles timeouts, parses the response, and provides error handling for failed requests or JSON parsing.

--- template/express-js/lib/dropio/server.js
+++ template/express-js/lib/dropio/server.js
@@ -174,6 +174,42 @@ class DIOApi {
       clearTimeout(timeoutId);
     }
   }
+  async bucketDetails() {
+    const controller = new AbortController();
+    const timeoutId = setTimeout(() => controller.abort(), 30000);
+
+    try {
+      const res = await fetch(`${process.env.DROPIO_INGEST_SERVER}/bucket/${process.env.DROPIO_APP_ID}`, {
+        method: 'GET',
+        cache: 'no-store',
+        headers: {
+          Authorization: `Bearer ${process.env.DROPIO_TOKEN}`,
+        },
+        signal: controller.signal,
+      });
+
+      let response = null;
+
+      try {
+        response = await res.json();
+      } catch (jsonError) {
+        console.error('Failed to parse JSON from get bucket details dioapi:', jsonError);
+        return { error: 'Failed to parse JSON from get bucket details' };
+      }
+
+      if (response?.code !== 200) {
+        console.log('INGEST_SERVER_ERORR:', response.errors);
+        return { error: response?.message };
+      }
+
+      return { success: response?.message };
+    } catch (error) {
+      console.error('Error get bucket details:', error);
+      return { error: 'Error During get bucket details' };
+    } finally {
+      clearTimeout(timeoutId);
+    }
+  }
 }
 
 exports.createDropio = createDropio;
